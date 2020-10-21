5dbd75728dc30d5147f4373affbd9fe9fa749be0
Mariam Dhifallah <45049205+MariamDhifalla@users.noreply.github.com>
2020-10-22 00:44:15 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI : "mongodb+srv://bouthouriMed:123456Aa@cluster0.swp2z.mongodb.net/checkpoint?retryWrites=true&w=majority"
+    mongoURI : "mongodb+srv://MariamDhifallah:134562mD@cluster0.swp2z.mongodb.net/checkpoint?retryWrites=true&w=majority"
 }
